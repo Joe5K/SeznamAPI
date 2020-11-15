50b30758faef9ca224017022ffe874db99d13069
Joe5K <jozo.krkosk4@gmail.com>
2020-11-15 16:45:10 +0100

    added string checking in validation

--- core/path_permission.py
+++ core/path_permission.py
@@ -6,6 +6,6 @@ from core.config import ALLOWED_PATH
 
 
 def permissed_path(value):
-    if not Path(value).is_relative_to(ALLOWED_PATH):
+    if type(value) != str or not Path(value).is_relative_to(ALLOWED_PATH):
         raise Forbidden
     return value
