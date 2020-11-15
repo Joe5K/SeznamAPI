a36c92309bbf0f55a4927de38c8b1f3841d65148
Joe5K <jozo.krkosk4@gmail.com>
2020-11-15 18:40:29 +0100

    rewrited function

--- core/path_permission.py
+++ core/path_permission.py
@@ -6,7 +6,8 @@ from core.config import ALLOWED_PATH
 
 
 def permitted_path(value):
-    normalized_path = os.path.realpath(value)
-    if type(value) != str or os.path.commonprefix((normalized_path, ALLOWED_PATH)) != ALLOWED_PATH:
-        raise Forbidden
-    return normalized_path
+    if type(value) == str:
+        normalized_path = os.path.realpath(value)
+        if os.path.commonprefix((normalized_path, ALLOWED_PATH)) == ALLOWED_PATH:
+            return normalized_path
+    raise Forbidden
